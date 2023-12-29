85e266207fb6c8d01e352176e5f45545df9d4787
Michael Lulev <miki1990@gmail.com>
2023-12-29 12:33:58 +0200

    Add getPaths and getShortestPath util functions

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,5 +1,6 @@
 
 
+
 export const SECOND = 1000
 export const MINUTE = 60 * SECOND
 export const HOUR = 60 * MINUTE
@@ -7,13 +8,26 @@ export const DAY = 24 * HOUR
 export const WEEK = 7 * DAY
 
 
-export function debounce(func, timeout=500) {
+export function debounce(func, delay=500) {
     let timeoutId
-    const debounceFunc = (...args) => {
+    return (...args) => {
         clearTimeout(timeoutId)
-        timeoutId = setTimeout(() => {
-            func.apply(this, args)
-        }, timeout)
+        timeoutId = setTimeout(func, delay, ...args)
     }
-    return debounceFunc
+}
+
+export function getShortestPath(...args) {
+    const paths = getPaths(...args)
+    return paths.reduce((shortestPath, path) => path.length < shortestPath.length ? path : shortestPath, paths[0])
+}
+
+export function getPaths(object, value, maxDepth=3, checkEqual=((a, b) => a === b), path='', paths=[], origObj=null) {
+    if (maxDepth < 0 || object === origObj) return
+    for (const key in object) {
+        const _path = path + `.${key}`
+        if (checkEqual(object[key], value)) paths.push(_path)
+        if (typeof object[key] !== 'object') continue
+        getPaths(object[key], value, maxDepth-1, checkEqual, _path, paths, object)
+    }
+    return paths
 }
